36e6722310edc01fb85fcb5c0250d4a6045785a6
Nikita Aniskovets <happyaggro@gmail.com>
2021-04-07 23:08:51 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ public class Main {
         int bonusScale = 20;
         int bonusMile = ticketCost / bonusScale;
 
-        System.out.println("При покупке билета стоимостью" + ticketCost + "руб., Ваш бонус составит" + bonusMile + "бонусных миль.");
+        System.out.println("При покупке билета стоимостью " + ticketCost + " руб., Ваш бонус составит " + bonusMile + " бонусных миль.");
     }
 }
