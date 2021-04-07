12587c38dc45393fb33baa5aea9e67b5a4e05a5f
Nikita Aniskovets <happyaggro@gmail.com>
2021-04-07 22:44:20 +0300

    add code

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,9 @@
 public class Main {
+    public static void main(String[] args) {
+        int ticketCost = 9500;
+        int bonusScale = 20;
+        int bonusMile = ticketCost / bonusScale;
+
+        System.out.println("При покупке билета стоимостью" + ticketCost + "руб., Ваш бонус составит" + bonusMile + "бонусных миль.");
+    }
 }
